f0c57b957b4a44e0c39349da96f04e7487e8973b
ToniGrbic <tonigrbic.5@gmail.com>
2025-01-18 16:29:48 +0100

    env var for api url

--- fesb-prisutnost-mobile/services/api.ts
+++ fesb-prisutnost-mobile/services/api.ts
@@ -3,7 +3,7 @@ import axios, { AxiosError, AxiosInstance, AxiosResponse } from "axios";
 
 /* const url =
   Platform.OS === "android" ? "http://10.0.2.2:8081" : "http://127.0.0.1:8081"; */
-const url = "https://8ea5d3bcf8a1.ngrok.app";
+const url = process.env.EXPO_PUBLIC_API_URL;
 
 const Api: AxiosInstance = axios.create({ baseURL: url + "/api" });
 
